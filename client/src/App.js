d2fc6bef3e5f2b87e4740e9ec282ee5d37d99c46
leryramos16 <leryramos16@gmail.com>
2025-02-15 21:54:53 +0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
 import Salon from "./components/salon"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Promos from "./pages/Promos"
+import Services from "./pages/Services"
+import Booking from "./pages/Booking"
+
+
 
 function App() {
   return (
-    <div>
+    <>
+     <div>
       <Salon/>
     </div>
+
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={Home} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/promos" element={<Promos />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/booking" element={<Booking />} />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+    </>
+   
+     
+    
+    
   );
 }
 
--- client/src/components/salon.js
+++ client/src/components/salon.js
@@ -7,14 +7,15 @@ import TiktokLogo from "./images/tiktok.png";
 import IgLogo from "./images/ig.png";
 import ModelPicture from "./images/model.jpg";
 
+
 function Salon() {
   return (
     <div>
-      <div class="header">
+      <div className="header">
         <div class="logo-container">
-            <img class="logo" src={LogoProfile}/>
+            <img className="logo" src={LogoProfile}/>
         </div>
-        <div class="navigation-bar">
+        <div className="navigation-bar">
             <a href="http://127.0.0.1:5500/capstone/salon.html"> <button>HOME</button></a>
             <a href="http://127.0.0.1:5500/capstone/promo.html"><button>PROMOS</button></a>
             <a href="http://127.0.0.1:5500/capstone/service.html"><button>SERVICES</button></a>
@@ -24,26 +25,26 @@ function Salon() {
         
     </div>
 
-    <div class="bio">
+    <div className="bio">
         With our skilled and experienced hairstylist you are sure to come out a star!
     </div>
 
-    <div class="social-bar">
-        <a href="https://web.facebook.com/celebstyles.hairsalon" target="_blank"/> <img src={FbLogo}/>
-        <a href="https://www.tiktok.com/@celebritystyleshairsalon" target="_blank"/><img src={TiktokLogo}/>
-        <a href="https://www.instagram.com/celebrity.styles.hair.salon/" target="_blank"/> <img src={IgLogo}/>
+    <div className="social-bar">
+        <button><a href="https://web.facebook.com/celebstyles.hairsalon" target="_blank"/> <img src={FbLogo}/></button>
+        <button><a href="https://www.tiktok.com/@celebritystyleshairsalon" target="_blank"/><img src={TiktokLogo}/></button>
+        <button><a href="https://www.instagram.com/celebrity.styles.hair.salon/" target="_blank"/> <img src={IgLogo}/></button> 
     </div>
 
-    <div class="model-container">
+    <div className="model-container">
         <img class="model-pic" src={ModelPicture}/>
         
-        <div class="message"><span class="letter-s">S</span>tart your dreams with Celebrity Styles Hair Salon
+        <div className="message"><span class="letter-s">S</span>tart your dreams with Celebrity Styles Hair Salon
             <a href="http://127.0.0.1:5500/capstone/booking.html"><button>Book an Apointment</button></a>
         </div>
         
     </div>
 
-    <div class="follow-link">
+    <div className="follow-link">
         <a href="https://www.tiktok.com/@celebritystyleshairsalon"><button class="follow-message">FOLLOW US ON TIKTOK</button></a>
         <a href="https://www.tiktok.com/@celebritystyleshairsalon"><button class="tiktok-user">@celebritystyleshairsalon</button></a>
     </div>
